ccfae360a35e0d4dd8657e971d1915b505a7ed53
hindawi <ahmedhindawi2001@gmail.com>
2023-06-23 13:07:10 +0300

    removed additional lines

--- contracts/contractEx1.sol
+++ contracts/contractEx1.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-
 contract Counter {
     // all the smart contract source code
     uint public count;
@@ -26,7 +25,4 @@ contract Counter {
         count -= 1;
         emit Decrement(count);
     }
-
-
-
 }
